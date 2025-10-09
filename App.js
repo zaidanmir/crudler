2acee84389d7de458abad844ad7fed2b4fc8c60b
zaidanmir <zaidan2440@gmail.com>
2025-10-09 13:15:08 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ModuleListScreen from './src/components/screens/ModuleListScreen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+export const App = () => {
+  // Initialisations --------------------
+  // State ------------------------------
+  // Handlers ---------------------------
+  // View -------------------------------
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='ModuleListScreen'>
+        <Stack.Screen
+          name='ModuleListScreen'
+          component={ModuleListScreen}
+          options={{ title: 'List modules' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
