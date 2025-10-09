24065923580ea07d660a24aeb5b8fd4c888e80c9
zaidanmir <zaidan2440@gmail.com>
2025-10-09 14:42:20 +0100

    Created the CRUDL screens

--- App.js
+++ App.js
@@ -1,25 +1,50 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ModuleListScreen from './src/components/screens/ModuleListScreen';
+import ModuleAddScreen from './src/components/screens/ModuleAddScreen';
+import ModuleViewScreen from './src/components/screens/ModuleViewScreen';
+import ModuleModifyScreen from './src/components/screens/ModuleModifyScreen';
 
 const Stack = createNativeStackNavigator();
 
 export const App = () => {
-  // Initialisations --------------------
-  // State ------------------------------
-  // Handlers ---------------------------
-  // View -------------------------------
+  // -----------------------------
+  // Initialisations
+  // State
+  // Handlers
+  // View
+  // -----------------------------
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='ModuleListScreen'>
+      <Stack.Navigator
+        initialRouteName="ModuleListScreen"
+        screenOptions={{
+          headerStyle: { backgroundColor: 'black' },
+          headerTintColor: 'white',
+        }}
+      >
         <Stack.Screen
-          name='ModuleListScreen'
+          name="ModuleListScreen"
           component={ModuleListScreen}
           options={{ title: 'List modules' }}
+        />
+        <Stack.Screen
+          name="ModuleAddScreen"
+          component={ModuleAddScreen}
+          options={{ title: 'Add module' }}
+        />
+        <Stack.Screen
+          name="ModuleViewScreen"
+          component={ModuleViewScreen}
+          options={{ title: 'View module' }}
+        />
+        <Stack.Screen
+          name="ModuleModifyScreen"
+          component={ModuleModifyScreen}
+          options={{ title: 'Modify module' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-export default App;
