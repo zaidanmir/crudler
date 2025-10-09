b4335dc95300a5b62f39372f8494de0e74f39a4d
zaidanmir <zaidan2440@gmail.com>
2025-10-09 14:50:11 +0100

    Create Screen.js

---
+++ src/components/layout/Screen.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+const Screen = ({ children }) => {
+  // ---------------------------
+  // Initialisations
+  // State
+  // Handlers
+  // View
+  // ---------------------------
+  return (
+    <View style={styles.screen}>
+      {children}
+      <StatusBar style='light' />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Screen;
