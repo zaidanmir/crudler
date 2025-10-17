ef41e27517f2726660862d75b5733751f2f2275f
zaidanmir <zaidan2440@gmail.com>
2025-10-17 15:20:26 +0100

    Activity 6

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -5,13 +5,14 @@ import ModuleList from '../entity/modules/ModuleList.js';
 import RenderCount from '../UI/RenderCount.js';
 import initialModules from '../../data/modules.js';
 
-const ModuleListScreen = () => {
+const ModuleListScreen = ({navigation}) => {
   // Initialisations -------------------------------
   
   // State -----------------------------------------
   const [modules, setModules] = useState(initialModules);
 
   // Handlers --------------------------------------
+  const handleSelect = () => navigation.navigate('ModuleViewScreen', { module });
   const handleDelete = (module) =>
     setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
 
@@ -19,7 +20,7 @@ const ModuleListScreen = () => {
   return (
     <Screen>
       <RenderCount />
-      <ModuleList modules={modules} onSelect={handleDelete} />
+      <ModuleList modules={modules} onSelect={handleSelect} />
     </Screen>
   );
 };
--- src/components/screens/ModuleViewScreen.js
+++ src/components/screens/ModuleViewScreen.js
@@ -1,14 +1,17 @@
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 import Screen from '../layout/Screen';
+import ModuleView from '../entity/modules/ModuleView';
 
-const ModuleViewScreen = () => {
-  // Initialisations -------------------------------
-  // State -----------------------------------------
-  // Handlers --------------------------------------
-  // View ------------------------------------------
+const ModuleViewScreen = ({ navigate, route }) => {
+  // Initialisations ---------------------------
+  const { module } = route.params;
+
+  // State -------------------------------------
+  // Handlers ----------------------------------
+  // View --------------------------------------
   return (
     <Screen>
-      <Text>View</Text>
+      <ModuleView module={module} />
     </Screen>
   );
 };
