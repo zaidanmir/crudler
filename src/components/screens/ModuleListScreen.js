68ab4d5bc21e64d8933368567e1a1f8667da6627
zaidanmir <zaidan2440@gmail.com>
2025-12-08 18:10:41 +0000

    Activity 15

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,5 +1,6 @@
-import { LogBox, StyleSheet, Text } from 'react-native';
+import { LogBox, Alert, StyleSheet, Text } from 'react-native';
 import useLoad from '../API/useLoad';
+import API from '../API/API';
 import RenderCount from '../UI/RenderCount.js';
 import Icons from '../UI/Icons.js';
 import { Button, ButtonTray } from '../UI/Button.js';
@@ -14,34 +15,44 @@ const ModuleListScreen = ({ navigation }) => {
 
   // State -------------------------------------
 
-  const [modules, setModules, isLoading, loadModules] = useLoad(modulesEndpoint);
+  const [modules, , isLoading, loadModules] = useLoad(modulesEndpoint);
 
   // Handlers ----------------------------------
-  const handleDelete = (module) =>
-    setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
-
-  const handleAdd = (module) => setModules( [...modules, module] );  
-
-  const handleModify = (updatedModule) =>
-    setModules(
-      modules.map((module) =>
-        module.ModuleID === updatedModule.ModuleID ? updatedModule : module
-      )
-    );
-
-  const onDelete = (module) => {
-    handleDelete(module);
-    navigation.goBack();
+  
+  const onAdd = async (module) => {
+    const result = await API.post(modulesEndpoint, module);
+    if (result.isSuccess) {
+      loadModules(modulesEndpoint);
+      navigation.goBack();
+    } else {
+      Alert.alert(result.message);
+    }
   };
-
-  const onAdd = (module) => {
-    handleAdd(module);
-    navigation.goBack();
+  
+  
+  const onDelete = async (module) => {
+    const deleteEndpoint = `${modulesEndpoint}/${module.ModuleID}`;
+    const result = await API.delete(deleteEndpoint, module);
+    if (result.isSuccess) {
+      loadModules(modulesEndpoint);
+      navigation.goBack();
+    } else {
+      Alert.alert(result.message);
+    }
   };
+  
 
-  const onModify = (module) => {
-    handleModify(module);
-    navigation.replace('ModuleViewScreen', {module, onDelete, onModify });
+
+
+  const onModify = async (module) => {
+    const putEndpoint = `${modulesEndpoint}/${module.ModuleID}`;
+    const result = await API.put(putEndpoint, module);
+    if (result.isSuccess) {
+      loadModules(modulesEndpoint);
+      navigation.navigate('ModuleViewScreen', {module, onDelete, onModify});
+    } else {
+      Alert.alert(result.message);
+    }
   };
 
   const gotoViewScreen = (module) => navigation.navigate('ModuleViewScreen', { module, onDelete, onModify });
